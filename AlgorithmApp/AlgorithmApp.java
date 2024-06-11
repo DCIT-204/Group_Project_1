a88211ac41789ec6ea50b5a4008b7e0fbba7ec56
“Albert <orgin11phile@gmail.com>
2024-06-11 22:16:05 +0000

    Included Selection Sort

--- AlgorithmApp/AlgorithmApp.java
+++ AlgorithmApp/AlgorithmApp.java
@@ -58,6 +58,24 @@ public class AlgorithmApp {
             }
         }
 
+        public static void selectionSort(int[] arr) {
+            int n = arr.length;
+            for (int i = 0; i < n - 1; i++) {
+              int minIndex = i;
+              for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                  minIndex = j;
+                }
+              }
+              int temp = arr[minIndex];
+              arr[minIndex] = arr[i];
+              arr[i] = temp;
+            }
+          }
+        
+          // Selection sort has a time complexity of O(n^2)
+          //Selection sort has a space complexity of O(1)
+
         private static int partition(int[] arr, int low, int high) {
             int pivot = arr[high];
             int i = (low - 1);
