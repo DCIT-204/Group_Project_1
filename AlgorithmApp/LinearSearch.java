4af8de0242fee61b88ae9438ca7f740c1e02a1f2
nerdfavour <favourackonu13@gmail.com>
2024-06-27 23:41:57 +0000

    created classes for search algs and some sort algs

---
+++ AlgorithmApp/LinearSearch.java
@@ -0,0 +1,13 @@
+package AlgorithmApp;
+// Linear Search Algorithm class
+public class LinearSearch{
+    // Linear Search Algorithm
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
